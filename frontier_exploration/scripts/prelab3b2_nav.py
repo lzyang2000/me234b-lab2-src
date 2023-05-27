345c5793988a60b1e40d05e74f0817d3943fa288
Gary Yang <lzyang@caltech.edu>
2023-05-26 22:35:54 -0700

    remove mpc_local_planner, add stage sim

--- frontier_exploration/scripts/prelab3b2_nav.py
+++ frontier_exploration/scripts/prelab3b2_nav.py
@@ -7,10 +7,11 @@ from geometry_msgs.msg import PoseStamped
 from nav_msgs.srv import GetPlan
 from nav_msgs.msg import Path
 
-class MapPub():
+class navigation():
     def __init__(self):
         self.map_pub = rospy.Publisher("map", OccupancyGrid, queue_size=1)
         self.map_sub = rospy.Subscriber("/mce234b/grid_published", Float32MultiArray, self.map_callback, queue_size=1)
+        self.map_pub = rospy.Publisher("/mce234b/grid_published", Float32MultiArray, queue_size=1)
         self.goal_sub = rospy.Subscriber("/mce234b/goal_pose_published", PoseStamped, self.goal_callback, queue_size=1)
         self.curr_sub = rospy.Subscriber("/optitrack/vrpn_client_node/mce234b_bot/pose", PoseStamped, self.curr_callback, queue_size=1)
         self.global_path_pub = rospy.Publisher("global_path", Path, queue_size=1)
@@ -25,7 +26,7 @@ class MapPub():
         # convert msg to numpy array
         map_np = np.array(msg.data).reshape((msg.layout.dim[0].size, msg.layout.dim[1].size))
         # set map
-        self.set_map(map_np, msg.layout.dim[0].stride, (msg.layout.dim[0].data[msg.layout.dim[0].size-1], msg.layout.dim[1].data[msg.layout.dim[1].size-1]))
+        self.set_map(map_np, 0.5, (0,0))
         # publish map
 
     def set_map(self, map_np, reso, origin):
@@ -46,7 +47,6 @@ class MapPub():
         for _ in range(0,i):
             map_np = np.repeat(map_np, 2, axis=0).repeat(2, axis=1)
         map_np = np.flipud(map_np)
-        print(map_np)
         map_np[map_np == 0] = 0
         map_np[map_np == 1] = 100
         self.map.header.frame_id = "flipper/map"
@@ -77,26 +77,42 @@ class MapPub():
             self.global_path_pub.publish(resp1.plan)
 
 if __name__ == '__main__':
-    # map_array = np.zeros((12,12))
+    map_array = np.zeros((13,13))
     # all boarder cells are obstacles
-    # map_array[0,:] = 1
-    # map_array[-1,:] = 1
-    # map_array[:,0] = 1
-    # map_array[:,-1] = 1
-    # # add obstacles
+    map_array[0,:] = 1
+    map_array[-1,:] = 1
+    map_array[:,0] = 1
+    map_array[:,-1] = 1
+    # add obstacles
     # map_array[1,1] = 1
     # map_array[4:11,1] = 1
     # map_array[1:9,4:6] = 1
     # map_array[1:3,8:11] = 1
     # map_array[6:8,8:10] = 1
     # map_array[8:10,9] = 1
-    # # print(map_array)
+    # add 5x5 obstacle
+    map_array[4:9,4:9] = 1
+    # print(map_array)
+    # convert map_array to float32multiarray
+    map_array_msg = Float32MultiArray()
+    map_array_msg.data = map_array.flatten().tolist()
+    map_array_msg.layout.dim.append(map_array_msg.dim[0])
+    map_array_msg.layout.dim[0].label = "height"
+    map_array_msg.layout.dim[0].size = map_array.shape[0]
+    map_array_msg.layout.dim[0].stride = map_array.shape[1]
+    map_array_msg.layout.dim.append(map_array_msg.dim[0])
+    map_array_msg.layout.dim[1].label = "width"
+    map_array_msg.layout.dim[1].size = map_array.shape[1]
+    map_array_msg.layout.dim[1].stride = map_array.shape[1]
+    # print(map_array_msg)
 
     rospy.init_node("map_pub")
-    map_pub = MapPub()
+    nav = navigation()
+    nav.map_pub.publish(map_array_msg)
+
     rospy.wait_for_service("/global_planner/planner/make_plan")
     rate = rospy.Rate(10)
     while rospy.is_shutdown() == False:
-        map_pub.publish()
-        map_pub.plan()
+        nav.publish()
+        nav.plan()
         rate.sleep()
